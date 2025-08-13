23bcac930e15d05db5ebd7608b3bd899b9bf62e4
Nicat Mammadov <nmemmedov0217@gmail.com>
2025-08-13 12:34:31 +0400

    brainwave.io

---
+++ js test/main.js
@@ -0,0 +1,66 @@
+// console.log("Hello, World!");
+// var a = 5;
+// var b = 10;
+// console.log(a);
+
+// var sum = a + b;
+// console.log(sum);
+
+//*! primitive data types in JavaScript:
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol (ES6)
+// 7. BigInt (ES11)
+
+// var fullname = "john doe";
+// var age = 42;
+// var isstudent = true;
+// var adress = undefined;
+// var email = null;
+
+//*! inprimitive data types in JavaScript:
+// 1. Object
+// 2. Array
+// 3. Function
+
+// var firstName = "John";
+// var surname = "Doe";
+// var fullname = firstName + " " + surname;
+// console.log(fullname);
+
+// var a = 5.6;
+// var b = 10.4;
+// var sum = a + b;
+// console.log(sum);
+
+//*! Arithmetic Operators in JavaScript:
+// +	Addition
+// -	Subtraction
+// *	Multiplication
+// **	Exponentiation (ES2016)
+// /	Division
+// %	Modulus (Division Remainder)
+// ++	Increment
+// --	Decrement
+
+var a = 5;
+var b = 10;
+var sum = a + b;
+console.log("Sum:", sum);
+var difference = b - a;
+console.log("Difference:", difference);
+var product = a * b;
+console.log("Product:", product);
+var quotient = a / b;
+console.log("Quotient:", quotient);
+var remainder = b % a;
+console.log("Remainder:", remainder);
+var power = a ** 2;
+console.log("Power:", power);
+a++;
+console.log("Incremented a:", a);
+b--;
+console.log("Decremented b:", b);
